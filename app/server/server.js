a73b63b697511215d356b29c4094c1244035fffe
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-12-14 23:09:53 +0000

    initial commit

--- app/server/server.js
+++ app/server/server.js
@@ -17,79 +17,82 @@ var db = require("./db");
 var routes = require("./routes");
 var cron = require("./cron");
 
-var app = koa();
 
-app.use(views('views', {
-  default : 'jade'
-}));
+module.exports = function barbakoa() {
 
-mountStatic("/assets", __dirname + '/../.assets');
-mountStatic("/assets", __dirname + '/../assets');
-mountStatic("/assets", __dirname + '/../client');
+  var app = koa();
+
+  app.use(views('views', {
+    default: 'jade'
+  }));
+
+  mountStatic("/assets", __dirname + '/../.assets');
+  mountStatic("/assets", __dirname + '/../assets');
+  mountStatic("/assets", __dirname + '/../client');
 
 
-if (config.get("logs.request")) {
-  app.use(require("koa-logger")());
-}
+  if (config.get("logs.request")) {
+    app.use(require("koa-logger")());
+  }
 
-app.use(require("./error")());
-app.use(koaBody());
-app.keys = config.keys;
-app.use(session());
-app.use(flash());
+  app.use(require("./error")());
+  app.use(koaBody());
+  app.keys = config.keys;
+  app.use(session(app));
+  app.use(flash());
 
 
 //locals
-app.use(function *(next) {
-  this.locals = {
-    _: _,
-    flash: this.flash,
-    csrf: this.csrf,
-    config: config,
-    requestPath: this.request.path,
-    routeUrl: routes.routeUrl
-  };
-  yield *next;
-});
+  app.use(function *(next) {
+    this.locals = {
+      _: _,
+      flash: this.flash,
+      csrf: this.csrf,
+      config: config,
+      requestPath: this.request.path,
+      routeUrl: routes.routeUrl
+    };
+    yield *next;
+  });
 
 //routes
-routes.configure(app);
+  routes.configure(app);
 
 
 // server methods
-var server;
-exports.start = function* () {
+  var server;
 
-  if (config.get("db.sync")) {
-    yield db.syncAllModels();
+  app.start = function () {
+    co(function * () {
+      if (config.get("db.sync")) {
+        yield db.syncAllModels();
+      }
+
+      server = app.listen(config.get("port"));
+
+      if (config.get("assets.reload")) {
+        var gulp = require("gulp");
+        require("../../gulpfile");
+        gulp.start("watch");
+      }
+    }).catch(function () {
+
+    });
+  };
+
+  app.stop = function () {
+    server.close();
+  };
+
+  function mountStatic(url, path) {
+    logger.info("mounting %s to %s", path, url);
+    var assets = koa();
+    assets.use(staticCache(path));
+    app.use(mount(url, assets));
   }
 
-  server = app.listen(config.get("port"));
-
-  if (config.get("assets.reload")) {
-    var gulp = require("gulp");
-    require("../../gulpfile");
-    gulp.start("watch");
-  }
+  return app;
 
 };
 
-exports.stop = function* () {
-  server.close();
-};
 
-
-//
-if (require.main === module) {
-  co(function* () {
-    yield exports.start()
-  })();
-}
-
-//
-function mountStatic(url, path) {
-  logger.info("mounting %s to %s", path, url);
-  var assets = koa();
-  assets.use(staticCache(path));
-  app.use(mount(url, assets));
-}
