737210e157cd89fb31989e9ab2103a80e2241682
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-12-29 19:12:06 +0000

    comment cron

--- app/server/cron.js
+++ app/server/cron.js
@@ -1,3 +1,4 @@
+/*
 var co = require("co");
 //var twitter = require("./lib/twitter");
 var cronJob = require('cron').CronJob;
@@ -8,4 +9,4 @@ new cronJob('00 * * * * *', function () {
   co(function* () {
     //yield twitter.cronTask();
   })();
-}, null, enableTwitterCron, "America/Los_Angeles");
+}, null, enableTwitterCron, "America/Los_Angeles");*/
