649da0f2662a9ceed3aee024807c324614187c20
Axel Hernández Ferrera <axelhzf@gmail.com>
2015-02-09 15:10:44 +0000

    support for modules without bower.json

--- app/server/assets.js
+++ app/server/assets.js
@@ -13,63 +13,67 @@ exports.initialize = function () {
     var gulp = require("gulp");
     var gulptasks = require("../../gulptasks");
     gulptasks(gulp);
-
+    
     gulp.on('err', function () {
       log.error("[assets] error");
     });
-
+    
     gulp.on('task_start', function (e) {
       log.debug({task: e.task}, 'start task');
     });
-
+    
     gulp.on('task_stop', function (e) {
       var time = prettyTime(e.hrDuration);
       log.debug({task: e.task, etime: time}, "end task");
     });
-
+    
     gulp.on('task_err', function (e) {
       var msg = e;
       var time = prettyTime(e.hrDuration);
       log.debug({task: e.task, etime: time}, "task error");
     });
-
+    
     gulp.on('task_not_found', function (err) {
       log.debug({task: e.task}, "task is not in your gulpfile");
     });
-
+    
     gulp.start("watch");
-
+    
   }
 };
 
 exports.expandModule = function (moduleName) {
   var pathApp = config.get("path.app");
   var m = require(path.join(pathApp, "app", "client", moduleName + ".json"));
-
+  
   var components = m.components.map(function (m) {
-    var moduleBase = path.join(pathApp, "app", "client", "components", m);
-    var bjson = require(path.join(moduleBase, "bower.json"));
-    var main = bjson.main;
-
-    if (_.isArray(main)) {
-      main = _.find(main, function (file) {
-        return _s.endsWith(file, ".js");
-      });
-    }
-
-    if (main) {
-      return path.join("components", m, main);
+    if (_s.endsWith(m, ".js")) {
+      return path.join("components", m);
     } else {
-      return "";
+      var moduleBase = path.join(pathApp, "app", "client", "components", m);
+      var bjson = require(path.join(moduleBase, "bower.json"));
+      var main = bjson.main;
+      
+      if (_.isArray(main)) {
+        main = _.find(main, function (file) {
+          return _s.endsWith(file, ".js");
+        });
+      }
+      
+      if (main) {
+        return path.join("components", m, main);
+      } else {
+        return "";
+      }
     }
   });
-
+  
   return {
     components: components,
     js: m.js,
     style: m.style
   };
-
+  
 };
 
 exports.getModule = function (moduleName) {
@@ -85,7 +89,7 @@ exports.getModule = function (moduleName) {
   } else {
     js = ["js/" + moduleName + ".js"]
   }
-
+  
   return {
     js: js,
     style: "style/" + moduleName + ".css"
