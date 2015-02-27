e8e5a7469d1b3004085d3648c4f0c47b90a24d66
Axel Hernández Ferrera <axelhzf@gmail.com>
2015-02-27 10:58:41 +0000

    early abort validations

--- app/server/validations.js
+++ app/server/validations.js
@@ -1,31 +1,22 @@
 var _ = require("underscore");
 var Joi = require("joi");
+var Promise = require("bluebird");
 
-function validate(value, schema) {
-  return function (cb) {
-    Joi.validate(value, schema, function (err, value) {
-      if (!err) {
-        cb(null, value)
-      } else {
-        cb(err);
-      }
-    });
-  }
-}
+var validate = Promise.promisify(Joi.validate);
 
 function* middleware(next) {
   var ctx = this;
   ctx.validateParams = function* (schema) {
     var params = _.extend({}, ctx.params);
-    return yield validate(params, schema);
+    return yield validate(params, schema, {abortEarly: false});
   };
   ctx.validateQuery = function* (schema) {
     var query = ctx.query;
-    return yield validate(query, schema);
+    return yield validate(query, schema, {abortEarly: false});
   };
   ctx.validateBody = function* (schema) {
     var body = ctx.request.body;
-    return yield validate(body, schema);
+    return yield validate(body, schema, {abortEarly: false});
   };
   yield next;
 }
